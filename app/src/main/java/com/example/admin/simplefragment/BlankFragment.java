2805b7e67f311ace59fdd99712df7641fc3bee24
Draldo <draldo_lopez@hotmail.com>
2016-07-27 16:15:01 -0400

    Reusing fragment

--- app/src/main/java/com/example/admin/simplefragment/BlankFragment.java
+++ app/src/main/java/com/example/admin/simplefragment/BlankFragment.java
@@ -1,12 +1,17 @@
 package com.example.admin.simplefragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class BlankFragment extends Fragment {
+
+    private static final String TAG = "BlankFragmentTAG_";
 
     public BlankFragment() {
         // Required empty public constructor
@@ -15,8 +20,38 @@ public class BlankFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: ");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank, container, false);
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG, "onAttach: ");
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, "onViewCreated: ");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG, "onDestroyView: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG, "onDetach: ");
+    }
 }
--- app/src/main/java/com/example/admin/simplefragment/MainActivity.java
+++ app/src/main/java/com/example/admin/simplefragment/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.admin.simplefragment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: ");
 
         if(findViewById(R.id.container) != null){
             if (savedInstanceState != null) {
@@ -17,7 +23,44 @@ public class MainActivity extends AppCompatActivity {
             }
 
             BlankFragment fragment = new BlankFragment();
+            BlankFragment fragment2 = new BlankFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.container,fragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.container,fragment2).commit();
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    public void doMagic(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
